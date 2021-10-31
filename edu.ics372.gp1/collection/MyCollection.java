5696cdd471fd51445a4d1635aa57ac2ebdaf7c38
Dilli Khatiwoda <dilli.khatiwoda@yahoo.com>
2021-10-31 15:39:21 -0500

    Created collection package and MyCollection class

---
+++ edu.ics372.gp1/collection/MyCollection.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package collection;
+
+/**
+ * @author Dilli
+ *
+ */
+public class MyCollection {
+
+	/**
+	 * 
+	 */
+	public MyCollection() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
